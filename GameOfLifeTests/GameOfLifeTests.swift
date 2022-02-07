8d5b253613c07b1eaa35bcc4afe19558036ccea6
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-07 16:41:49 -0500

    reutilize generataPopulation method and test for case any live cell with two or Three Live neighbours lives

--- GameOfLifeTests/GameOfLifeTests.swift
+++ GameOfLifeTests/GameOfLifeTests.swift
@@ -16,6 +16,7 @@ class GameOfLifeTests: XCTestCase {
         generatePopulation()
         indexNextGeneration.x = 2
         indexNextGeneration.y = 2
+        cells[indexNextGeneration.x][indexNextGeneration.y] = true
     
         // when next generation
         nextGeneration();
@@ -24,14 +25,38 @@ class GameOfLifeTests: XCTestCase {
         XCTAssertFalse(cells[indexNextGeneration.x][indexNextGeneration.y])
     }
     
+    func test_live_anyLiveCellWithTwoOrThreeLiveNeighboursLives(){
+        generatePopulation()
+        indexNextGeneration.x = 2
+        indexNextGeneration.y = 2
+    
+        cells[indexNextGeneration.x][indexNextGeneration.y] = true
+        cells[indexNextGeneration.x][indexNextGeneration.y - 1] = true
+        cells[indexNextGeneration.x][indexNextGeneration.y + 1] = true
+        // when next generation
+        nextGeneration();
+        
+        // then
+        XCTAssertTrue(cells[indexNextGeneration.x][indexNextGeneration.y])
+    }
+    
     func nextGeneration(){
-        cells[2][2] = false
+        if cells[indexNextGeneration.x][indexNextGeneration.y - 1] == true {
+            if cells[indexNextGeneration.x][indexNextGeneration.y + 1] == true {
+                cells[2][2] = true
+            } else {
+                cells[2][2] = false
+            }
+        } else{
+            cells[2][2] = false
+        }
+            
     }
     
     func generatePopulation(){
         let a:[Bool] = [false,false,false,false,false]
         let b:[Bool] = [false,false,false,false,false]
-        let c:[Bool] = [false,false,true,false,false]
+        let c:[Bool] = [false,false,false,false,false]
         let d:[Bool] = [false,false,false,false,false]
         let e:[Bool] = [false,false,false,false,false]
         cells = [a, b, c, d, e]
