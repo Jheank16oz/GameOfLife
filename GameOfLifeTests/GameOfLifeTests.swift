5b913a6fdf729b526b37bb7deee4e5f48e79e86b
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-07 16:20:17 -0500

    Refactor magic numbers and initial cells

--- GameOfLifeTests/GameOfLifeTests.swift
+++ GameOfLifeTests/GameOfLifeTests.swift
@@ -10,29 +10,31 @@ import XCTest
 
 class GameOfLifeTests: XCTestCase {
     var cells = [[Bool]]()
-    
+    var indexNextGeneration = (x: 0, y: 0)
     func test_dies_anyLiveCellWithFewerThanTwoLiveNeighboursDies() {
         //given live cell with fewer than two live neigbors
+        generatePopulation()
+        indexNextGeneration.x = 2
+        indexNextGeneration.y = 2
+    
+        // when next generation
+        nextGeneration();
+        
+        // then
+        XCTAssertFalse(cells[indexNextGeneration.x][indexNextGeneration.y])
+    }
+    
+    func nextGeneration(){
+        cells[2][2] = false
+    }
+    
+    func generatePopulation(){
         let a:[Bool] = [false,false,false,false,false]
         let b:[Bool] = [false,false,false,false,false]
         let c:[Bool] = [false,false,true,false,false]
         let d:[Bool] = [false,false,false,false,false]
         let e:[Bool] = [false,false,false,false,false]
         cells = [a, b, c, d, e]
-        
-        let currentRow = 2
-        let currentColumn = 2
-    
-        
-        // when next generation
-        nextGeneration();
-        
-        // then
-        XCTAssertFalse(cells[currentRow][currentColumn])
-    }
-    
-    func nextGeneration(){
-        cells[2][2] = false
     }
 
 }
