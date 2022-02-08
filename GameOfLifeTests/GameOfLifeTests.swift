10d6f3d15183fd470befff5a71c2a34ecadd4749
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-07 19:53:58 -0500

    Add logic of count neighbours

--- GameOfLifeTests/GameOfLifeTests.swift
+++ GameOfLifeTests/GameOfLifeTests.swift
@@ -15,47 +15,87 @@ class GameOfLifeTests: XCTestCase {
     
     func test_dies_anyLiveCellWithFewerThanTwoLiveNeighboursDies() {
         //given live cell with fewer than two live neigbors
-        let game = GameOfLife(cells: generatePopulation())
-        let states = [LiveNeighbours.zeroAlive, LiveNeighbours.oneAlive]
+        let game = GameOfLife(cells: thereIsNoLife)
+        let states = [
+            LiveNeighbours.zeroAlive,
+            LiveNeighbours.oneAliveTop,
+            LiveNeighbours.oneAliveDown,
+            LiveNeighbours.oneAliveLeft,
+            LiveNeighbours.oneAliveRight,
+            LiveNeighbours.oneAliveTopRight,
+            LiveNeighbours.oneAliveTopLeft,
+            LiveNeighbours.oneAliveDownRight,
+            LiveNeighbours.oneAliveDownLeft,
+        ]
         
         for state in states {
             game.initialState(state: state)
+            print(game.cells)
+            print("\n")
             game.nextGeneration()
             XCTAssertEqual(game.cells, thereIsNoLife)
+            game.extermination()
         }
     }
     
     
     
-    func generatePopulation() ->[[Bool]]{
-        let a:[Bool] = [false,false,false,false,false]
-        let b:[Bool] = [false,false,false,false,false]
-        let c:[Bool] = [false,false,false,false,false]
-        let d:[Bool] = [false,false,false,false,false]
-        let e:[Bool] = [false,false,false,false,false]
-        
-        return [a, b, c, d, e]
-    }
+    
+    
 }
 
 enum LiveNeighbours {
     case zeroAlive
-    case oneAlive
+    case oneAliveTop
+    case oneAliveDown
+    case oneAliveLeft
+    case oneAliveRight
+    case oneAliveTopRight
+    case oneAliveTopLeft
+    case oneAliveDownRight
+    case oneAliveDownLeft
 }
 extension GameOfLife {
     
     func initialState(state:LiveNeighbours){
+        cells[2][2] = true
+        
         switch state {
         case .zeroAlive:
             break
-        case .oneAlive:
-            cells[2][2] = true
+        case .oneAliveTop:
+            cells[1][2] = true
             break
-        default:
+        case .oneAliveDown:
+            cells[3][2] = true
+            break
+        case .oneAliveLeft:
+            cells[2][1] = true
+            break
+        case .oneAliveRight:
+            cells[2][3] = true
+            break
+        case .oneAliveTopRight:
+            cells[1][3] = true
+            break
+        case .oneAliveTopLeft:
+            cells[1][1] = true
+            break
+        case .oneAliveDownRight:
+            cells[3][1] = true
+            break
+        case .oneAliveDownLeft:
+            cells[3][3] = true
             break
         }
     }
+    
+    func extermination(){
+        cells = thereIsNoLife
+    }
 }
+
+    
 
 let a:[Bool] = [false,false,false,false,false]
 let b:[Bool] = [false,false,false,false,false]
