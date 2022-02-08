a07f9fead510f78cac9bb45d32b0abd8fa7ceae4
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-08 10:21:23 -0500

    start new approach because  i am coding production code without test

--- GameOfLife/GameOfLife.swift
+++ GameOfLife/GameOfLife.swift
@@ -9,43 +9,5 @@ import Foundation
 
 final class GameOfLife {
     
-    var cells:[[Bool]]
-    
-    init(cells:[[Bool]]){
-        self.cells = cells
-    }
-         
-    func nextGeneration(){
-        
-        for (rowIndex, rows) in cells.enumerated() {
-            for (columnIndex, cell) in rows.enumerated() {
-                if cell {
-                    let neighbourTop = cells[rowIndex - 1][columnIndex]
-                    let neighbourDown = cells[rowIndex + 1][columnIndex]
-                    let neighbourLeft = cells[rowIndex][columnIndex - 1]
-                    let neighbourRight = cells[rowIndex][columnIndex + 1]
-                    let neighbourTopLeft = cells[rowIndex - 1][columnIndex - 1]
-                    let neighbourTopRight = cells[rowIndex - 1][columnIndex + 1]
-                    let neighbourDownLeft = cells[rowIndex + 1 ][columnIndex - 1]
-                    let neighbourDownRight = cells[rowIndex + 1 ][columnIndex + 1]
-                    
-                    let count = sum(neighBours: neighbourTop, neighbourDown, neighbourLeft, neighbourRight, neighbourTopLeft, neighbourTopRight, neighbourDownLeft, neighbourDownRight)
-                    
-                    if count < 2 {
-                        cells[rowIndex][columnIndex] = false
-                    }
-                }
-                    
-            }
-                
-        }
-    }
-    
-    func sum(neighBours:Bool...) -> Int{
-        var sum: Int = 0
-        for n in neighBours {
-            if n {  sum += 1}
-        }
-        return sum
-    }
+  
 }
