c0f5261182d3f384ad9b71aaefea6c833d2d046e
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-13 17:29:43 -0500

    Game of life test on mac view

--- UI-Mac/ViewController.swift
+++ UI-Mac/ViewController.swift
@@ -6,13 +6,30 @@
 //
 
 import Cocoa
+import GameOfLife
 
 class ViewController: NSViewController {
 
+    
+    @IBOutlet weak var labelPrint: NSTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let builder = GameOfLifeStarter.Builder()
+        builder.seed = .rpentomino20x20
+        builder.generationCount = 100
+        builder.update = {  [weak self] cells in
+            guard let `self` = self else { return }
+            self.labelPrint.stringValue = self.printSeed(seed: cells)
+        }
+        do {
+            let game = try builder.build()
+            game.start()
+            
+        } catch {
+            print("Errror initializing")
+        }
+        
     }
 
     override var representedObject: Any? {
@@ -20,6 +37,27 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    func printSeed(seed:[[State]]) -> String{
+        var value = ""
+        for (_,row) in seed.enumerated() {
+            var rowString = ""
+            for (_,col) in row.enumerated() {
+                //let state = col == State.alive ? "🌸" : "🌿"
+                let state = col == State.alive ? "🤡" : "💤"
+                //let state = col == State.alive ? "🐇" : "🐾"
+                //let state = col == State.alive ? "🧝" : "🧟‍♂️"
+                //let state = col == State.alive ? "🕷" : "🕸"
+                //let state = col == State.alive ? "❤️" : "🤍"
+                rowString += "\(state)"
+            }
+            value += "\(rowString)\n"
+        }
+        return value
+            
+    }
+    
+    
 
 
 }
